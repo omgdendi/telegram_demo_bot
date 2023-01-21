41954b2164a8faf9536540fefb8000f716573cc4
omgdendi <omgdendi@mail.ru>
2023-01-21 18:27:09 +0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import {ConfigService} from "./confirg/config.service";
+import {IConfigService} from "./confirg/config.interface";
+import {Telegraf} from "telegraf";
+import {IBotContext} from "./context/context.interface";
+import {Command} from "./commands/command.class";
+import {StartCommand} from "./commands/start.command";
+import LocalSession from "telegraf-session-local";
+
+class Bot {
+    bot: Telegraf<IBotContext>;
+    commands: Command[] = [];
+
+    constructor(private readonly configService: IConfigService) {
+        this.bot = new Telegraf<IBotContext>(this.configService.get("TOKEN"));
+        this.bot.use(
+            (new LocalSession({ database: 'sessions.json' })).middleware()
+        )
+    }
+
+    init() {
+        this.commands = [new StartCommand(this.bot)]
+        for (const command of this.commands)
+            command.handle();
+        this.bot.launch();
+    }
+}
+
+const bot = new Bot(new ConfigService());
---
+++ src/commands/start.command.ts
@@ -0,0 +1,30 @@
+import {Command} from "./command.class";
+import {Markup, Telegraf} from "telegraf";
+import {IBotContext} from "../context/context.interface";
+
+export class StartCommand extends Command {
+    constructor(bot: Telegraf<IBotContext>) {
+        super(bot);
+    }
+
+    handle(): void {
+        this.bot.start((ctx) => {
+            console.log(ctx.session);
+            ctx.reply("Вам понравился курс?",
+                Markup.inlineKeyboard([
+                    Markup.button.callback("👍", "course_like"),
+                    Markup.button.callback("👎", "course_dislike")
+                ]))
+        })
+
+        this.bot.action("course_like", (ctx) => {
+            ctx.session.courseLike = true;
+            ctx.editMessageText("Круто!");
+        })
+
+        this.bot.action("course_dislike", (ctx) => {
+            ctx.session.courseLike = false;
+            ctx.editMessageText("🙁");
+        })
+    }
+}
